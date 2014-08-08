97b5167110d20903274ac65f604920eafecb9bed
Kramspill <m-a-p@hotmail.co.uk>
2014-08-08 12:16:31 +0100

    Initial class definition of the states that a Card can be in.

--- State/CardStates.h
+++ State/CardStates.h
@@ -3,16 +3,100 @@
 
 #include <QState>
 
-class CardStates : public QState
+class InDeckState : public QState
 {
-    Q_OBJECT
 public:
-    explicit CardStates(QObject *parent = 0);
+    InDeckState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
 
-signals:
+protected:
+    void onEntry(QEvent*) {}
+};
 
-public slots:
+class InPlayerHandState : public QState
+{
+public:
+    InPlayerHandState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
 
+protected:
+    void onEntry(QEvent*) {}
+};
+
+class InCpuHandState : public QState
+{
+public:
+    InCpuHandState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
+
+protected:
+    void onEntry(QEvent*) {}
+};
+
+class InTalonState : public QState
+{
+public:
+    InTalonState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
+
+protected:
+    void onEntry(QEvent*) {}
+};
+
+class InPlayerDiscardsState : public QState
+{
+public:
+    InPlayerDiscardsState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
+
+protected:
+    void onEntry(QEvent*) {}
+};
+
+class InCpuDiscardsState : public QState
+{
+public:
+    InCpuDiscardsState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
+
+protected:
+    void onEntry(QEvent*) {}
+};
+
+class InCurrentTrickState : public QState
+{
+public:
+    InCurrentTrickState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
+
+protected:
+    void onEntry(QEvent*) {}
+};
+
+class InPreviousTricksState : public QState
+{
+public:
+    InPreviousTricksState(QState* parent = 0) :
+        QState(parent)
+    {
+    }
+
+protected:
+    void onEntry(QEvent*) {}
 };
 
 #endif // CARDSTATES_H
