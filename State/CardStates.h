83015cc7dd036cdcd49e5b2dc99f52264e3cae87
Kramspill <m-a-p@hotmail.co.uk>
2014-08-08 12:14:48 +0100

    Add CardStates.h that represents all the states a card can be in and rename the phases of the game to represent (and inherit from) a QState.

---
+++ State/CardStates.h
@@ -0,0 +1,18 @@
+#ifndef CARDSTATES_H
+#define CARDSTATES_H
+
+#include <QState>
+
+class CardStates : public QState
+{
+    Q_OBJECT
+public:
+    explicit CardStates(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // CARDSTATES_H
