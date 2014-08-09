23b819b3ddad61857fe0a790db61239813b2c860
Kramspill <m-a-p@hotmail.co.uk>
2014-08-09 10:22:38 +0100

    Initial implementation of DealPhaseState added.

--- State/DealPhaseState.cpp
+++ State/DealPhaseState.cpp
@@ -7,8 +7,8 @@ DealPhaseState::DealPhaseState(QState* parent) :
 }
 
 // Copy Constructor.
-DealPhaseState::DealPhaseState(DealPhaseState& other) :
-    QState(other)
+DealPhaseState::DealPhaseState(DealPhaseState&) :
+    QState()
 {
 }
 
@@ -17,12 +17,64 @@ DealPhaseState::~DealPhaseState(void)
 {
 }
 
+// Initialize and execute the internal state machine of
+// the DealPhaseState.
 void DealPhaseState::onEntry(QEvent*)
+{
+    // Initialize the state machine.
+    stateMachine = new QStateMachine();
+
+    // Initialize the states within the state machine.
+    QState*      initialState = new QState(stateMachine);
+    QState*      dealToPlayer = new QState(stateMachine);
+    QState*      dealToCpu    = new QState(stateMachine);
+    QState*      dealTalon    = new QState(stateMachine);
+    QFinalState* finalState   = new QFinalState(stateMachine);
+
+    // Set the initial state for the state machine.
+    stateMachine->setInitialState(initialState);
+
+    /*
+    // Setup the transitions from initialState.
+    initialState->addTransition(SomeObject, SIGNAL(clicked()), dealToPlayer);
+    initialState->addTransition(SomeObject, SIGNAL(clicked()), dealToCpu);
+
+    // Setup the transitions from dealToPlayer.
+    dealToPlayer->addTransition(SomeObject, SIGNAL(clicked()), dealToCpu);
+    dealToPlayer->addTransition(SomeObject, SIGNAL(clicked()), dealTalon);
+
+    // Setup the transitions from dealToCpu.
+    dealToCpu->addTransition(SomeObject, SIGNAL(clicked()), dealToPlayer);
+    dealToCpu->addTransition(SomeObject, SIGNAL(clicked()), dealTalon);
+
+    // Setup the transitions from dealTalon.
+    dealTalon->addTransition(SomeObject, SIGNAL(clicked()), finalState);
+    */
+
+    /*
+    // Setup the work done in each state.
+    connect(dealToPlayer, SIGNAL(entered()), this, SLOT(DealToPlayer()));
+    connect(dealToCpu,    SIGNAL(entered()), this, SLOT(DealToCpu()));
+    connect(dealTalon,    SIGNAL(entered()), this, SLOT(DealTalon()));
+    */
+}
+
+void DealPhaseState::onExit(QEvent*)
 {
 
 }
 
-void DealPhaseState::onExit(QEvent*)
+void DealPhaseState::DealToPlayer(void)
+{
+
+}
+
+void DealPhaseState::DealToCpu(void)
+{
+
+}
+
+void DealPhaseState::DealTalon(void)
 {
 
 }
--- State/DealPhaseState.h
+++ State/DealPhaseState.h
@@ -2,18 +2,28 @@
 #define DEALPHASESTATE_H
 
 #include <QState>
+#include <QFinalState>
 #include <QEvent>
+#include <QStateMachine>
 
 class DealPhaseState : public QState
 {
 public:
     DealPhaseState(QState* parent = 0);
-    DealPhaseState(DealPhaseState& other);
+    DealPhaseState(DealPhaseState&);
     ~DealPhaseState(void);
 
 protected:
     void onEntry(QEvent*);
     void onExit(QEvent*);
+
+private slots:
+    void DealToPlayer(void);
+    void DealToCpu(void);
+    void DealTalon(void);
+
+private:
+    QStateMachine* stateMachine;
 };
 
 #endif // DEALPHASESTATE_H
