a22429e5743c9f9f09e43c0fe319211e24e8474c
Kramspill <m-a-p@hotmail.co.uk>
2014-08-11 17:05:09 +0100

    Clean up Main.cpp.

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,31 @@
+//------------------------------------------------------------------------------
+// Filename: Main.cpp
+// Description: Entry point for the application.
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Qt Header Files
+//------------------------------------------------------------------------------
 #include <QApplication>
+
+
+//------------------------------------------------------------------------------
+// My Header Files
+//------------------------------------------------------------------------------
 #include "Managers/GameManager.h"
 
+
+//------------------------------------------------------------------------------
+// main - Entry point for the application.
+//------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
     // Manages the GUI application's control flow and main settings.
-    // For any GUI application using Qt, there is precisely one QApplication object.
+    // For any GUI application using Qt, there is precisely one
+    // QApplication object.
     QApplication piquetGame(argc, argv);
 
-    // Manages all other managers in the application.
+    // Top layer manager that starts off the initialization process.
     GameManager* gameManager = new GameManager();
     gameManager->Initialize();
 
