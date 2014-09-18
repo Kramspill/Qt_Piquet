46995cf01dd88d517cd2ebfb021a97f33e84e78b
Kramspill <m-a-p@hotmail.co.uk>
2014-09-18 13:04:36 +0100

    ScoreManager  - Added additional enums for scoring and responses.

--- Managers/ScoreManager.h
+++ Managers/ScoreManager.h
@@ -13,11 +13,42 @@
 
 
 //------------------------------------------------------------------------------
+// My Header Files
+//------------------------------------------------------------------------------
+#include "Cards/Card.h"
+
+
+//------------------------------------------------------------------------------
 // Class: ScoreManager
 //------------------------------------------------------------------------------
 class ScoreManager : public QObject
 {
     Q_OBJECT
+public:
+    enum Response
+    {
+        GOOD,
+        NOTGOOD,
+        EVEN,
+        QUESTION
+    };
+
+    enum SequenceScoring
+    {
+        THREE = 3,
+        FOUR  = 4,
+        FIVE  = 15,
+        SIX   = 16,
+        SEVEN = 17,
+        EIGHT = 18
+    };
+
+    struct PhaseScore
+    {
+        int numOfCards;
+        int totalValue;
+    };
+
 public:
     ScoreManager(void);
     ScoreManager(ScoreManager&);
