a23607107eab7412f92209e0675b02f28e5c8941
Kramspill <m-a-p@hotmail.co.uk>
2014-09-25 10:22:38 +0100

    Dialog  - Add stubs for functions.

--- Scene/Dialog.h
+++ Scene/Dialog.h
@@ -19,7 +19,24 @@ class Dialog : public QDialog
 {
     Q_OBJECT
 public:
+    enum ButtonTypes
+    {
+        DEAL,
+        EXCHANGE,
+        DECLARE,
+        PASS,
+        GOOD,
+        NOTGOOD,
+        OK
+    };
+
+public:
     explicit Dialog(QWidget *parent = 0);
+    Dialog(Dialog&);
+    ~Dialog(void);
+
+    void Initialize(char* title, char* message, int numOfBtns,
+                    ButtonType* buttonTypes);
 
 signals:
 
