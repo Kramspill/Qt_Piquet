5b90182da31932fc94e7a0aaa334a065531086b0
Kramspill <m-a-p@hotmail.co.uk>
2014-10-05 15:03:07 +0100

    CardLayout  - Add an initial implementation based off the code found here: http://qt-project.org/doc/qt-5/layout.html

--- Layout/CardLayout.cpp
+++ Layout/CardLayout.cpp
@@ -12,7 +12,8 @@
 //------------------------------------------------------------------------------
 // Constructor
 //------------------------------------------------------------------------------
-CardLayout::CardLayout(void)
+CardLayout::CardLayout(QWidget* parent) :
+    QLayout(parent)
 {
 }
 
@@ -20,7 +21,8 @@ CardLayout::CardLayout(void)
 //------------------------------------------------------------------------------
 // Copy Constructor
 //------------------------------------------------------------------------------
-CardLayout::CardLayout(CardLayout&)
+CardLayout::CardLayout(CardLayout&) :
+    QLayout()
 {
 }
 
@@ -30,4 +32,97 @@ CardLayout::CardLayout(CardLayout&)
 //------------------------------------------------------------------------------
 CardLayout::~CardLayout(void)
 {
+    Destroy();
+}
+
+
+//------------------------------------------------------------------------------
+// Destroy - Cleans up the memory allocated by this layout.
+//------------------------------------------------------------------------------
+void CardLayout::Destroy(void)
+{
+    QLayoutItem *item;
+
+    while ( item = takeAt(0) )
+        delete item;
+}
+
+
+//------------------------------------------------------------------------------
+// addItem - Add items to this layout.
+//------------------------------------------------------------------------------
+void CardLayout::addItem(QLayoutItem* item)
+{
+    items.append(item);
+}
+
+
+//------------------------------------------------------------------------------
+// setGeometry - How the layout is defined.
+//------------------------------------------------------------------------------
+void CardLayout::setGeometry(const QRect& rect)
+{
+    QLayout::setGeometry(rect);
+
+    if (items.size() == 0)
+        return;
+
+    int width  = rect.width()  - (items.size() - 1) * spacing();
+    int height = rect.height() - (items.size() - 1) * spacing();
+
+    int index = 0;
+    while ( index < items.size() )
+    {
+        QLayoutItem* item = items.at(index);
+        QRect geometry(rect.x() + index * spacing(),
+                       rect.y() + index * spacing(),
+                       width,
+                       height);
+
+        items->setGeometry(geometry);
+        index++;
+    }
+}
+
+
+//------------------------------------------------------------------------------
+// sizeHint - The preferred size of this layout.
+//------------------------------------------------------------------------------
+QSize CardLayout::sizeHint(void) const
+{
+    QSize size(0,0);
+
+    int numOfItems = items.size();
+    if (numOfItems > 0)
+        size = QSize(100,70);
+
+    int index = 0;
+    while ( index < numOfItems )
+    {
+        QLayoutItem* item = items.at(index);
+        size = size.expandedTo(item->sizeHint());
+        index++;
+    }
+
+    return size + numOfItems * QSize(spacing(), spacing());
+}
+
+
+//------------------------------------------------------------------------------
+// itemAt - Retrieve a layout item at the specified index.
+//------------------------------------------------------------------------------
+QLayoutItem* CardLayout::itemAt(int index) const
+{
+    return items.value(index);
+}
+
+
+//------------------------------------------------------------------------------
+// takeAt - Remove a layout item at the given index.
+//------------------------------------------------------------------------------
+QLayoutItem* CardLayout::takeAt(int index)
+{
+    bool validRange = index >= 0 && index < items.size();
+
+    return validRange ? items.takeAt(index) : 0;
 }
--- Layout/CardLayout.h
+++ Layout/CardLayout.h
@@ -7,14 +7,31 @@
 #define CARDLAYOUT_H
 
 //------------------------------------------------------------------------------
+// Qt Header Files
+//------------------------------------------------------------------------------
+#include <QLayout>
+
+
+//------------------------------------------------------------------------------
 // Class: CardLayout
 //------------------------------------------------------------------------------
-class CardLayout
+class CardLayout : public QLayout
 {
 public:
-    CardLayout(void);
+    CardLayout(QWidget* parent);
     CardLayout(CardLayout&);
     ~CardLayout(void);
+
+    void                      Destroy(void);
+
+    void                      addItem(QLayoutItem* item);
+    void                      setGeometry(const QRect& rect);
+    QSize                     sizeHint(void) const;
+    QLayoutItem*              itemAt(int index) const;
+    QLayoutItem*              takeAt(int index);
+
+private:
+    QList<QLayoutItem*> items;
 };
 
 #endif // CARDLAYOUT_H
